78b56b2b1034ccb21c3344c0eb0fab36b830e857
Alex Malcolm <alexjamesmalcolm@gmail.com>
2018-02-09 13:49:35 -0500

    RoboticDogTest: shouldHaveHealth100

--- src/main/java/virtualpet/RoboticDog.java
+++ src/main/java/virtualpet/RoboticDog.java
@@ -2,15 +2,38 @@ package virtualpet;
 
 public class RoboticDog {
 
+	private String name;
+	private int dryness;
+	private String description;
+
 	public RoboticDog(String name, String description) {
+		this.name = name;
+		this.description = description;
 	}
 
 	public RoboticDog(String name, String description, int dryness, int health) {
+		this.dryness = dryness;
 	}
 
 	public int getHealth() {
 		// TODO Auto-generated method stub
 		return 100;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public int getDryness() {
+		return dryness;
+	}
+
+	public void oil() {
+		dryness = 0;
 	}
 
 }
