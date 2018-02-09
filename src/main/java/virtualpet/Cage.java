4a2e1097771afe47597eb1a3042b123c30d0dd54
Alex Malcolm <alexjamesmalcolm@gmail.com>
2018-02-09 10:29:44 -0500

    CageTest: shouldHaveDirtynessLevelBe69

--- src/main/java/virtualpet/Cage.java
+++ src/main/java/virtualpet/Cage.java
@@ -3,9 +3,14 @@ package virtualpet;
 public class Cage {
 
 	public static final int DEFAULT_DIRTYNESS = 0;
+	private int dirtyness;
+
+	public Cage(int dirtyness) {
+		this.dirtyness = dirtyness;
+	}
 
 	public int getDirtyness() {
-		return 0;
+		return dirtyness;
 	}
 
 }
--- src/test/java/virtualpet/CageTest.java
+++ src/test/java/virtualpet/CageTest.java
@@ -2,6 +2,7 @@ package virtualpet;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
+import static virtualpet.Cage.DEFAULT_DIRTYNESS;
 
 import org.junit.Test;
 
@@ -9,8 +10,15 @@ public class CageTest {
 	
 	@Test
 	public void shouldHaveDirtynessLevelDefault() {
-		Cage underTest = new Cage();
-		int cleanliness = underTest.getDirtyness();
-		assertThat(cleanliness,is(Cage.DEFAULT_DIRTYNESS));
+		Cage underTest = new Cage(DEFAULT_DIRTYNESS);
+		int dirtyness = underTest.getDirtyness();
+		assertThat(dirtyness,is(DEFAULT_DIRTYNESS));
+	}
+	
+	@Test
+	public void shouldHaveDirtynessLevelBe69() {
+		Cage underTest = new Cage(69);
+		int dirtyness = underTest.getDirtyness();
+		assertThat(dirtyness, is(69));
 	}
 }
