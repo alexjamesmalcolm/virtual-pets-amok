e0206c00fd4d49e5f2168974213ebfb73fc9b7e9
Alex Malcolm <alexjamesmalcolm@gmail.com>
2018-02-11 16:59:40 -0500

    VirtualPetShelterTest: shouldWalkOrganicDog

---
+++ src/main/java/virtualpet/VirtualPetShelter.java
@@ -0,0 +1,25 @@
+package virtualpet;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class VirtualPetShelter {
+	
+	Map<String, Pet> pets = new HashMap<>();
+
+	public Pet getPet(String name) {
+		return pets.get(name);
+	}
+
+	public VirtualPetShelter(LitterBox litterBox) {
+	}
+
+	public void add(Pet pet) {
+		pets.put(pet.getName(), pet);
+	}
+
+	public void walk() {
+		((Walkable) pets.get("Joey")).walk();
+	}
+
+}
--- src/test/java/virtualpet/VirtualPetShelterTest.java
+++ src/test/java/virtualpet/VirtualPetShelterTest.java
@@ -1,10 +1,40 @@
 package virtualpet;
 
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class VirtualPetShelterTest {
 
+	private VirtualPetShelter petShelter;
+	private LitterBox litterBox;
+	private OrganicDog organicDog;
+	private RoboticDog roboticDog;
+	private OrganicCat organicCat;
+	private RoboticCat roboticCat;
+
+	@Before
+	public void setup() {
+		litterBox = new LitterBox();
+		petShelter = new VirtualPetShelter(litterBox);
+		organicDog = new OrganicDog("Joey", "Cool OrganicDog");
+		organicCat = new OrganicCat("Phil", "Cool OrganicCat", litterBox);
+		roboticDog = new RoboticDog("Auto", "Chill RoboticDog");
+		roboticCat = new RoboticCat("Synth", "Chill RoboticCat");
+	}
+	
 	@Test
-	public void should() {
+	public void shouldWalkOrganicDog() {
+		petShelter.add(organicDog);
+		petShelter.add(organicCat);
+		petShelter.add(roboticDog);
+		petShelter.add(roboticCat);
+		petShelter.walk();
+		Pet pet = petShelter.getPet("Joey");
+		assertThat(pet.getBoredom(), is(0));
 	}
 }
