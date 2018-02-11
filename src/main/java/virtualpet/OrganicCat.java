15ea155c5b453ebe2a995a8d22dc255af53266a6
Alex Malcolm <alexjamesmalcolm@gmail.com>
2018-02-11 16:14:55 -0500

    OrganicCatTest: shouldHaveLitterBox

--- src/main/java/virtualpet/OrganicCat.java
+++ src/main/java/virtualpet/OrganicCat.java
@@ -2,17 +2,24 @@ package virtualpet;
 
 public class OrganicCat extends Organic {
 
-	public OrganicCat(String name, String description) {
-		this(name, description, DEFAULT_BOREDOM, 20, 30, 40);
+	private LitterBox litterBox;
+
+	public OrganicCat(String name, String description, LitterBox litterBox) {
+		this(name, description, DEFAULT_BOREDOM, 20, 30, 40, litterBox);
 	}
 
-	public OrganicCat(String name, String description, int boredom, int hunger, int thirst, int waste) {
+	public OrganicCat(String name, String description, int boredom, int hunger, int thirst, int waste, LitterBox litterBox) {
 		this.name = name;
 		this.description = description;
 		this.boredom = boredom;
 		this.hunger = hunger;
 		this.thirst = thirst;
 		this.waste = waste;
+		this.litterBox = litterBox;
+	}
+
+	public LitterBox getLitterBox() {
+		return litterBox;
 	}
 
 }
--- src/test/java/virtualpet/OrganicCatTest.java
+++ src/test/java/virtualpet/OrganicCatTest.java
@@ -3,61 +3,71 @@ package virtualpet;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class OrganicCatTest {
 
+	private LitterBox litterBox;
+
+	@Before
+	public void setup() {
+		litterBox = new LitterBox();
+	}
+
 	@Test
 	public void shouldHaveName() {
-		OrganicCat underTest = new OrganicCat("Joey", "Cool");
+		OrganicCat underTest = new OrganicCat("Joey", "Cool", litterBox);
 		String name = underTest.getName();
 		assertThat(name, is("Joey"));
 	}
 
 	@Test
 	public void shouldHaveDescription() {
-		OrganicCat underTest = new OrganicCat("Joey", "Cool");
+		OrganicCat underTest = new OrganicCat("Joey", "Cool", litterBox);
 		String description = underTest.getDescription();
 		assertThat(description, is("Cool"));
 	}
 
 	@Test
 	public void shouldHaveBoredom() {
-		OrganicCat underTest = new OrganicCat("Joey", "Cool", 10, 20, 30, 40);
+		OrganicCat underTest = new OrganicCat("Joey", "Cool", 10, 20, 30, 40, litterBox);
 		int boredom = underTest.getBoredom();
 		assertThat(boredom, is(10));
 	}
 
 	@Test
 	public void shouldHaveCustomName() {
-		OrganicCat underTest = new OrganicCat("Joey", "Cool", 10, 20, 30, 40);
+		OrganicCat underTest = new OrganicCat("Joey", "Cool", 10, 20, 30, 40, litterBox);
 		String name = underTest.getName();
 		assertThat(name, is("Joey"));
 	}
 
 	@Test
 	public void shouldHaveHunger() {
-		OrganicCat underTest = new OrganicCat("Joey", "Cool", 10, 20, 30, 40);
+		OrganicCat underTest = new OrganicCat("Joey", "Cool", 10, 20, 30, 40, litterBox);
 		int hunger = underTest.getHunger();
 		assertThat(hunger, is(20));
 	}
-	
+
 	@Test
 	public void shouldHaveThirst() {
-		OrganicCat underTest = new OrganicCat("Joey", "Cool", 10, 20, 30, 40);
+		OrganicCat underTest = new OrganicCat("Joey", "Cool", 10, 20, 30, 40, litterBox);
 		int thirst = underTest.getThirst();
 		assertThat(thirst, is(30));
 	}
-	
+
 	@Test
 	public void shouldHaveWaste() {
-		OrganicCat underTest = new OrganicCat("Joey", "Cool", 10, 20, 30, 40);
+		OrganicCat underTest = new OrganicCat("Joey", "Cool", 10, 20, 30, 40, litterBox);
 		int waste = underTest.getWaste();
 		assertThat(waste, is(40));
 	}
-	
-//	@Test
-//	public void shouldHaveLitterBox() {
-//		OrganicCat underTest = new OrganicCat("Joey", "Cool", LitterBox litterBox);
-//	}
+
+	@Test
+	public void shouldHaveLitterBox() {
+		OrganicCat underTest = new OrganicCat("Joey", "Cool", litterBox);
+		LitterBox check = underTest.getLitterBox();
+		assertThat(check instanceof LitterBox, is(true));
+	}
 }
