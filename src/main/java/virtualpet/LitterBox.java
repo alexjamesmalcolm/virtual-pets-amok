3b11427839fc5201f85247dd289d19473b989244
Alex Malcolm <alexjamesmalcolm@gmail.com>
2018-02-11 16:03:40 -0500

    Formatted LitterBox

--- src/main/java/virtualpet/LitterBox.java
+++ src/main/java/virtualpet/LitterBox.java
@@ -1,11 +1,11 @@
 package virtualpet;
 
 public class LitterBox extends Box {
-	
+
 	public LitterBox() {
 		this(DEFAULT_DIRTYNESS);
 	}
-	
+
 	public LitterBox(int dirtyness) {
 		this.dirtyness = dirtyness;
 	}
